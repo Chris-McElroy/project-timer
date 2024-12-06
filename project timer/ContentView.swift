6d058bdadd7e9eaad07a9f1c39b325601197d1bd
ChrisMcElroy <csmcelroy@me.com>
2024-12-05 21:33:57 -0500

    Initial Commit

---
+++ project timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project timer
+//
+//  Created by 4 on '24.12.5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project timer/project_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_timerApp.swift
+//  project timer
+//
+//  Created by 4 on '24.12.5.
+//
+
+import SwiftUI
+
+@main
+struct project_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
